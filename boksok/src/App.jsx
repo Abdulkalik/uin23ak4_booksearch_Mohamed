5c6d7c1dde0387ac9d4c6ea956bbd3ea3f36f995
Abdulkalik <abdi16310@gmail.com>
2024-04-05 12:14:36 +0200

    update på app.css, app.jsx

--- boksok/src/App.jsx
+++ boksok/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Layout from './components/Layout';
+import SearchBar from './components/SearchBar';
+import BookCard from './components/BookCard';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (searchResults) => {
+    setSearchResults(searchResults);
+  };
 
   return (
-    <>
+    <Layout>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <SearchBar onSearch={handleSearch} />
+        {searchResults.map(book => (
+          <BookCard key={book.key} book={book} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Layout>
+  );
 }
 
-export default App
+export default App;
